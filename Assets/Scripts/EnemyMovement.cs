bcccfa61b4e3576c9ffcb336652e1ed95ba34563
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-11-05 14:29:05 +0100

    FlipEnemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed = 5f; // Mozg�si sebess�g
-    private Vector2 direction = Vector2.right; // Kezdeti ir�ny
+    private Vector2 direction = Vector2.left; // Kezdeti ir�ny
     private float timer = 0f; // Id�z�t�
     private Rigidbody2D rb;
+    private bool PlayerSpotted = false;
 
     private void Start()
     {
@@ -17,20 +18,31 @@ public class EnemyMovement : MonoBehaviour
 
     private void Update()
     {
+        if(PlayerSpotted) speed = 10f;
+        else speed = 5f;
         // N�velj�k az id�z�t�t a deltaTime-mal (az el�z� frame �ta eltelt id�)
         timer += Time.deltaTime;
 
         // Ha eltelt 5 m�sodperc, megford�tjuk az ir�nyt �s lenull�zzuk az id�z�t�t
-        if (timer >= 3f)
-        {
-            direction = -direction;
-            timer = 0f;
+        if(!PlayerSpotted){
+            if (timer >= 3f){
+                direction = -direction;
+                Flip();
+                timer = 0f;
+            }
         }
+        
     }
 
     private void FixedUpdate()
     {
         // Mozgatjuk az objektumot a Rigidbody2D-vel az aktu�lis ir�nyban
         rb.velocity = direction * speed;
+    }
+
+    private void Flip(){
+        Vector3 ls = transform.localScale;
+                ls.x *= -1f;
+                transform.localScale = ls;
     }
 }
