31b06088a094c8c3fd78342af0e310218994dfb0
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-11-08 11:51:37 +0100

    SetupMenu

--- Assets/Scripts/Menu_UI_Manager.cs
+++ Assets/Scripts/Menu_UI_Manager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Menu_UI_Manager : MonoBehaviour
 {
+    [SerializeField] Canvas NewPlayer;
+    [SerializeField] Canvas Login;
+    [SerializeField] Canvas LoggedIn;
+
+    [SerializeField] Button btnLogin;
+    [SerializeField] Button btnEnter;
+    [SerializeField] Button btnNewEndless;
+    [SerializeField] Button btnLoginEndless;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,15 @@ public class Menu_UI_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void LoginActive(){
+        NewPlayer.SetActive(false);
+        Login.SetActive(true);
+    }
+
+    void LoggedInActive(){
+        Login.SetActive(false);
+        LoggedIn.SetActive(true);
     }
 }
