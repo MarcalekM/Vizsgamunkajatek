5491162c035ecfc0a51fabbd583131fffde299fb
Csizmadia Márk <airamek@teamorange.hu>
2025-01-23 01:01:52 +0100

    QOL patch

--- Assets/Scripts/Enemy/Enemies/Goblin.cs
+++ Assets/Scripts/Enemy/Enemies/Goblin.cs
@@ -9,7 +9,6 @@ public class Goblin : Enemy
         if (IsGrounded(transform.position + new Vector3(direction.x, direction.y, 0) * hSpeed) || PlayerSpotted)
         {
             var force = direction * hSpeed;
-            Debug.Log(rb.velocity.magnitude);
             if (PlayerSpotted && rb.velocity.magnitude < 0.001)
             {
                 rb.velocity = new Vector2(rb.velocity.x, hSpeed);
