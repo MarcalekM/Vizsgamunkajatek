e45571a04788f76e4ec68c00c56e1d51f5499df2
Bevíz László <bevizlaci@gmail.com>
2025-01-28 19:59:34 +0100

    arena

---
+++ Assets/Scripts/ArenaController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+
+    [SerializeField] private GameObject Goblin;
+    [SerializeField] private GameObject Ghost;
+    [SerializeField] private GameObject Golem;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (Player.activeSelf)
+        {
+            yield return new WaitForSeconds(4f);
+
+            for (int i = 0; i < 2; i++)
+            {
+                GameObject enemyPrefab = null;
+                int rnd = Random.Range(0, 9);
+
+                if (rnd <= 3)
+                    enemyPrefab = Goblin;
+                else if (rnd > 3 && rnd <= 6)
+                    enemyPrefab = Ghost;
+                else
+                    enemyPrefab = Golem;
+
+                Vector2 playerPosition = Player.GetComponent<BoxCollider2D>().bounds.center;
+
+                float spawnOffsetX = Random.Range(7f, 9f);
+                bool spawnOnLeft = Random.value > 0.5f;
+
+                if (spawnOnLeft)
+                    spawnOffsetX *= -1;
+
+                Vector2 spawnPosition = new Vector2(playerPosition.x + spawnOffsetX, playerPosition.y);
+
+                GameObject enemyInstance = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+                enemyInstance.transform.localScale = enemyPrefab.transform.localScale;
+
+                if (spawnOnLeft)
+                    enemyInstance.transform.localScale = new Vector3(-enemyInstance.transform.localScale.x,
+                                                                     enemyInstance.transform.localScale.y,
+                                                                     enemyInstance.transform.localScale.z);
+            }
+
+
+        }
+    }
+}
