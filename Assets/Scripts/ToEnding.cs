aeb5d002c7b38a3b3f52f782b1af6777f3555729
Bevíz László <bevizlaci@gmail.com>
2025-01-28 09:11:10 +0100

    boss fix

--- Assets/Scripts/ToEnding.cs
+++ Assets/Scripts/ToEnding.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToEnding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        sceneManager.LoadScene("End");
-    }
+    void Start() => SceneManager.LoadScene("End");
 }
